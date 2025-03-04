f076c35aefb593b782f0a9c5a877e08386296917
Рубановский Влад <vlad.rubanovskiy@gmail.com>
2025-03-04 17:49:11 +0200

    add main idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'test';
+  private currentRotation = 0;
+  sectors = [10, 20, 30, 40, 50, 60];
+  selectedValue: number | null = null;
+  animateResult = false;
+
+  get rotationStyle() {
+    return `rotate(${this.currentRotation}deg)`;
+  }
+
+  spinWheel() {
+    const randomDegrees = 1440 + Math.floor(Math.random() * 720);
+    this.currentRotation += randomDegrees;
+    this.animateResult = false;
+    setTimeout(() => {
+      const finalRotation = this.currentRotation % 360;
+      let sectorIndex = Math.floor((360 - finalRotation) / 60) % 6;
+      this.selectedValue = this.sectors[sectorIndex];
+      this.animateResult = true;
+    }, 5000);
+  }
 }
